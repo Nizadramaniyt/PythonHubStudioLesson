9ecafadcbed101c601ad17f6772e618aabb51c03
Nizadramaniyt <alexei.konsky@gmail.com>
2023-12-25 18:28:16 +0700

    added goods app, connected catalog & product.html

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
 from main import views
 
-app_name = 'name'
+app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
